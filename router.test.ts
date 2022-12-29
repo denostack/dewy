c7936e42285a34db758dd6c3e4689eadce09e5b1
Changwan Jun <wan2land@gmail.com>
2022-12-29 20:50:34 +0900

    feat: add router middleware

--- router.test.ts
+++ router.test.ts
@@ -1,22 +1,245 @@
-import { assertEquals } from "testing/asserts.ts";
-import { createRouter } from "./router.ts";
+import { assertEquals, assertInstanceOf, fail } from "testing/asserts.ts";
+import { ServerError } from "./error/server_error.ts";
+import { Router } from "./router.ts";
+
+async function assert404(router: Router, request: Request) {
+  try {
+    await router.dispatch(request);
+    fail();
+  } catch (e) {
+    assertInstanceOf(e, ServerError);
+    assertEquals(e.status, 404);
+    assertEquals(e.message, "Not Found");
+  }
+}
 
 Deno.test("router, empty", async () => {
-  const router = createRouter();
+  const router = new Router();
 
-  const response = await router(new Request("https://example.local"));
-
-  assertEquals(response.status, 404);
-  assertEquals(await response.text(), "Not Found");
+  await assert404(router, new Request("https://example.local"));
 });
 
-Deno.test("router, simple", async () => {
-  const router = createRouter();
+Deno.test("router, simple rule", async () => {
+  const router = new Router();
 
-  router.addRoute("GET", "/", () => new Response("pong", { status: 200 }));
+  new URLPattern({});
+  router.addRoute({
+    methods: ["GET"],
+    pattern: new URLPattern({ pathname: "/" }),
+    fn: () => new Response("pong", { status: 200 }),
+  });
+  router.addRoute({
+    methods: ["GET", "POST"],
+    pattern: new URLPattern({ pathname: "/users/:id" }),
+    fn: ({ match }) =>
+      new Response(`user ${match.pathname.groups.id}`, { status: 200 }),
+  });
 
-  const response = await router(new Request("https://example.local"));
+  {
+    const response = await router.dispatch(
+      new Request("https://example.local", { method: "GET" }),
+    );
 
-  assertEquals(response.status, 200);
-  assertEquals(await response.text(), "pong");
+    assertEquals(response.status, 200);
+    assertEquals(await response.text(), "pong");
+  }
+  {
+    const response = await router.dispatch(
+      new Request("https://example.local/users/123", { method: "post" }),
+    );
+
+    assertEquals(response.status, 200);
+    assertEquals(await response.text(), "user 123");
+  }
+});
+
+Deno.test("router, get, post, ... methods", async () => {
+  const router = new Router();
+
+  new URLPattern({});
+  router.get("/get", () => new Response("get", { status: 200 }));
+  router.head("/head", () => new Response("head", { status: 200 }));
+  router.post("/post", () => new Response("post", { status: 200 }));
+  router.put("/put", () => new Response("put", { status: 200 }));
+  router.delete("/delete", () => new Response("delete", { status: 200 }));
+  router.options("/options", () => new Response("options", { status: 200 }));
+  router.patch("/patch", () => new Response("patch", { status: 200 }));
+  router.any("/any", () => new Response("any", { status: 200 }));
+
+  // get
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/get", { method: "GET" }),
+    ).then((r) => r.text()),
+    "get",
+  );
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/get", { method: "HEAD" }),
+    ).then((r) => r.text()),
+    "get",
+  );
+  for (const method of ["POST", "PUT", "DELETE", "OPTIONS", "PATCH"]) {
+    await assert404(
+      router,
+      new Request("https://example.local/get", { method }),
+    );
+  }
+
+  // head
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/head", { method: "HEAD" }),
+    ).then((r) => r.text()),
+    "head",
+  );
+  for (const method of ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]) {
+    await assert404(
+      router,
+      new Request("https://example.local/head", { method }),
+    );
+  }
+
+  // post
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/post", { method: "POST" }),
+    ).then((r) => r.text()),
+    "post",
+  );
+  for (const method of ["GET", "HEAD", "PUT", "DELETE", "OPTIONS", "PATCH"]) {
+    await assert404(
+      router,
+      new Request("https://example.local/post", { method }),
+    );
+  }
+
+  // put
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/put", { method: "PUT" }),
+    ).then((r) => r.text()),
+    "put",
+  );
+  for (const method of ["GET", "HEAD", "POST", "DELETE", "OPTIONS", "PATCH"]) {
+    await assert404(
+      router,
+      new Request("https://example.local/put", { method }),
+    );
+  }
+
+  // delete
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/delete", { method: "DELETE" }),
+    ).then((r) => r.text()),
+    "delete",
+  );
+  for (const method of ["GET", "HEAD", "POST", "PUT", "OPTIONS", "PATCH"]) {
+    await assert404(
+      router,
+      new Request("https://example.local/delete", { method }),
+    );
+  }
+
+  // options
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/options", { method: "OPTIONS" }),
+    ).then((r) => r.text()),
+    "options",
+  );
+  for (const method of ["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"]) {
+    await assert404(
+      router,
+      new Request("https://example.local/options", { method }),
+    );
+  }
+
+  // patch
+  assertEquals(
+    await router.dispatch(
+      new Request("https://example.local/patch", { method: "PATCH" }),
+    ).then((r) => r.text()),
+    "patch",
+  );
+  for (const method of ["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"]) {
+    await assert404(
+      router,
+      new Request("https://example.local/patch", { method }),
+    );
+  }
+});
+
+Deno.test("router, middleware", async () => {
+  const router = new Router();
+
+  router.addRoute({
+    methods: ["GET"],
+    pattern: new URLPattern({ pathname: "/" }),
+    middlewares: [
+      ({ request }, next) => {
+        const auth = request.headers.get("Authorization");
+        if (!auth) {
+          return new Response("Unauthorized", { status: 401 });
+        }
+        return next();
+      },
+    ],
+    fn: () => new Response("pong", { status: 200 }),
+  });
+
+  {
+    const response = await router.dispatch(
+      new Request("https://example.local", { method: "GET" }),
+    );
+
+    assertEquals(response.status, 401);
+  }
+  {
+    const response = await router.dispatch(
+      new Request("https://example.local", {
+        method: "GET",
+        headers: { Authorization: "TOKEN" },
+      }),
+    );
+
+    assertEquals(response.status, 200);
+  }
+});
+
+Deno.test("router, use (predefined middlewares)", async () => {
+  const router = new Router();
+
+  router.use(({ request }, next) => {
+    const auth = request.headers.get("Authorization");
+    if (!auth) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    return next();
+  });
+
+  router.addRoute({
+    methods: ["GET"],
+    pattern: new URLPattern({ pathname: "/" }),
+    fn: () => new Response("pong", { status: 200 }),
+  });
+
+  {
+    const response = await router.dispatch(
+      new Request("https://example.local", { method: "GET" }),
+    );
+
+    assertEquals(response.status, 401);
+  }
+  {
+    const response = await router.dispatch(
+      new Request("https://example.local", {
+        method: "GET",
+        headers: { Authorization: "TOKEN" },
+      }),
+    );
+
+    assertEquals(response.status, 200);
+  }
 });
--- router.ts
+++ router.ts
@@ -7,69 +7,164 @@ export interface Context {
 
 export type RouteHandler = (ctx: Context) => Response | Promise<Response>;
 
-export interface Router {
-  (request: Request): Promise<Response>;
+export type MiddlewareHandler = (
+  ctx: Context,
+  next: (ctx?: Context) => Response | Promise<Response>,
+) => Response | Promise<Response>;
 
-  addRoute(method: string, path: string, fn: RouteHandler): void;
-  addRoute(method: string, pattern: URLPatternInput, fn: RouteHandler): void;
-  addRoute(method: string, pattern: URLPattern, fn: RouteHandler): void;
+export interface RouteParams {
+  methods: string[];
+  pattern: URLPattern;
+  middlewares?: MiddlewareHandler[];
+  fn: RouteHandler;
+}
+
+function pattern(pathOrPattern: string | URLPatternInput | URLPattern) {
+  if (typeof pathOrPattern === "string") {
+    return new URLPattern({ pathname: pathOrPattern });
+  }
+  if (pathOrPattern instanceof URLPattern) {
+    return pathOrPattern;
+  }
+  return new URLPattern(pathOrPattern);
+}
+
+export class Router {
+  definedMiddlewares: MiddlewareHandler[] = [];
+  routes = new Map<
+    string,
+    [
+      pattern: URLPattern,
+      handler: RouteHandler,
+      middlewares: MiddlewareHandler[],
+    ][]
+  >();
+
+  use(...middlewares: MiddlewareHandler[]) {
+    this.definedMiddlewares.push(...middlewares);
+  }
+
+  get(path: string, fn: RouteHandler): void;
+  get(pattern: URLPatternInput, fn: RouteHandler): void;
+  get(pattern: URLPattern, fn: RouteHandler): void;
+  get(pathOrPattern: string | URLPatternInput | URLPattern, fn: RouteHandler) {
+    this.addRoute({
+      methods: ["get", "head"],
+      pattern: pattern(pathOrPattern),
+      fn,
+    });
+  }
+
+  head(path: string, fn: RouteHandler): void;
+  head(pattern: URLPatternInput, fn: RouteHandler): void;
+  head(pattern: URLPattern, fn: RouteHandler): void;
+  head(pathOrPattern: string | URLPatternInput | URLPattern, fn: RouteHandler) {
+    this.addRoute({ methods: ["head"], pattern: pattern(pathOrPattern), fn });
+  }
+
+  post(path: string, fn: RouteHandler): void;
+  post(pattern: URLPatternInput, fn: RouteHandler): void;
+  post(pattern: URLPattern, fn: RouteHandler): void;
+  post(pathOrPattern: string | URLPatternInput | URLPattern, fn: RouteHandler) {
+    this.addRoute({ methods: ["post"], pattern: pattern(pathOrPattern), fn });
+  }
+
+  put(path: string, fn: RouteHandler): void;
+  put(pattern: URLPatternInput, fn: RouteHandler): void;
+  put(pattern: URLPattern, fn: RouteHandler): void;
+  put(pathOrPattern: string | URLPatternInput | URLPattern, fn: RouteHandler) {
+    this.addRoute({ methods: ["put"], pattern: pattern(pathOrPattern), fn });
+  }
+
+  delete(path: string, fn: RouteHandler): void;
+  delete(pattern: URLPatternInput, fn: RouteHandler): void;
+  delete(pattern: URLPattern, fn: RouteHandler): void;
+  delete(
+    pathOrPattern: string | URLPatternInput | URLPattern,
+    fn: RouteHandler,
+  ) {
+    this.addRoute({ methods: ["delete"], pattern: pattern(pathOrPattern), fn });
+  }
+
+  options(path: string, fn: RouteHandler): void;
+  options(pattern: URLPatternInput, fn: RouteHandler): void;
+  options(pattern: URLPattern, fn: RouteHandler): void;
+  options(
+    pathOrPattern: string | URLPatternInput | URLPattern,
+    fn: RouteHandler,
+  ) {
+    this.addRoute({
+      methods: ["options"],
+      pattern: pattern(pathOrPattern),
+      fn,
+    });
+  }
+
+  patch(path: string, fn: RouteHandler): void;
+  patch(pattern: URLPatternInput, fn: RouteHandler): void;
+  patch(pattern: URLPattern, fn: RouteHandler): void;
+  patch(
+    pathOrPattern: string | URLPatternInput | URLPattern,
+    fn: RouteHandler,
+  ) {
+    this.addRoute({ methods: ["patch"], pattern: pattern(pathOrPattern), fn });
+  }
+
+  any(path: string, fn: RouteHandler): void;
+  any(pattern: URLPatternInput, fn: RouteHandler): void;
+  any(pattern: URLPattern, fn: RouteHandler): void;
+  any(
+    pathOrPattern: string | URLPatternInput | URLPattern,
+    fn: RouteHandler,
+  ) {
+    this.addRoute({
+      methods: ["get", "head", "post", "put", "delete", "options", "patch"],
+      pattern: pattern(pathOrPattern),
+      fn,
+    });
+  }
+
+  addRoute({ methods, pattern, fn, middlewares }: RouteParams) {
+    for (let method of methods) {
+      method = method.trim().toLowerCase();
+      let routeGroup = this.routes.get(method);
+      if (!routeGroup) {
+        routeGroup = [];
+        this.routes.set(method, routeGroup);
+      }
+      routeGroup.push([pattern, fn, [
+        ...this.definedMiddlewares,
+        ...middlewares ?? [],
+      ]]);
+    }
+  }
+
+  async dispatch(request: Request): Promise<Response> {
+    async function execute(
+      middlewares: MiddlewareHandler[],
+      fn: RouteHandler,
+      ctx: Context,
+    ): Promise<Response> {
+      if (middlewares.length === 0) {
+        return await fn(ctx);
+      }
+      const [middleware, ...nextMiddlewares] = middlewares;
+      return await middleware(ctx, async (nextCtx) => {
+        return await execute(nextMiddlewares, fn, nextCtx ?? ctx);
+      });
+    }
+
+    const routes = this.routes.get(request.method.toLowerCase()) ?? [];
+    for (const [pattern, fn, middlewares] of routes) {
+      const match = pattern.exec(request.url);
+      if (match) {
+        return await execute(middlewares, fn, { match, request });
+      }
+    }
+    throw new ServerError("Not Found", 404);
+  }
 }
 
 export interface CreateRouterOptions {
   errorHandler?: (error: Error) => Response | Promise<Response>;
-}
-
-const defaultErrorHandler = (error: Error) => {
-  if (error instanceof ServerError) {
-    return new Response(error.message, { status: error.status });
-  }
-  return new Response(error.message, { status: 500 });
-};
-
-export function createRouter(options: CreateRouterOptions = {}): Router {
-  const routes = new Map<URLPattern, Map<string, RouteHandler>>();
-
-  const errorHandler = options.errorHandler ?? defaultErrorHandler;
-
-  async function route(request: Request): Promise<Response> {
-    for (const [pattern, group] of routes) {
-      const fn = group.get(request.method.toLowerCase());
-      if (!fn) {
-        continue;
-      }
-      const match = pattern.exec(request.url);
-      if (match) {
-        try {
-          return await fn({ match, request });
-        } catch (e) {
-          return errorHandler(e);
-        }
-      }
-    }
-    return errorHandler(new ServerError("Not Found", 404));
-  }
-
-  return Object.assign(route, {
-    addRoute(
-      method: string,
-      pathOrPattern: string | URLPatternInput | URLPattern,
-      fn: RouteHandler,
-    ) {
-      let pattern: URLPattern;
-      if (typeof pathOrPattern === "string") {
-        pattern = new URLPattern({ pathname: pathOrPattern });
-      } else if (pathOrPattern instanceof URLPattern) {
-        pattern = pathOrPattern;
-      } else {
-        pattern = new URLPattern(pathOrPattern);
-      }
-
-      let routeGroup = routes.get(pattern);
-      if (!routeGroup) {
-        routeGroup = new Map<string, RouteHandler>();
-        routes.set(pattern, routeGroup);
-      }
-      routeGroup.set(method.trim().toLowerCase(), fn);
-    },
-  });
 }
